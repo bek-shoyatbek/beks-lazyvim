76fa8f8e6e6c15df1eb5af2956855d021fe75ed1
bek-shoyatbek <shoyatbekov03032003@gmail.com>
2025-02-08 18:50:29 +0500

    feat: hower signature on 'M' added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,8 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Add LSP hover keymap
+vim.keymap.set("n", "M", function()
+  vim.lsp.buf.hover()
+end, { desc = "Show hover information", noremap = true, silent = true })
